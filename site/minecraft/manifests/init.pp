11c21853f0d06d91007ff5d40809f1b8126a830e
Michael Leech <michael.leech@physics.ox.ac.uk>
2019-12-11 16:51:15 +0000

    fix init minecraft.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,14 +2,14 @@ class minecraft {
   file {'/opt/minecraft':
     ensure => directory,
   }
-  file {/opt/minecraft/minecraft_server.jar:
+  file {'/opt/minecraft/minecraft_server.jar':
     ensure => file,
     source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   }
   package {'java':
     ensure => present,
   }
-  file {'/opt/minecraft/eula.txt:
+  file {'/opt/minecraft/eula.txt':
     ensure => file,
     content => 'eula=true',
   }
