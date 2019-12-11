3c41344d00443ed63737ecebdcb15993596edc56
Michael Leech <michael.leech@physics.ox.ac.uk>
2019-12-11 11:52:49 +0000

    modify /root/readme

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,7 @@ node 'puppetserver.localdomain' {
   include role::master_server
   file { '/root/README':
     ensure => file,
-    content => "Welcome to {$fqdn} zap\n",
+    content => "Welcome to ${fqdn} zap\n",
   }
 }
 
